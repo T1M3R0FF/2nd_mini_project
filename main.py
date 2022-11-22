b1828c77eef718bebc60638ef7b4ea48b05b6697
aleksandr <alexandrl33t@yandex.ru>
2022-11-22 09:06:23 +0300

    мои правки

--- main.py
+++ main.py
@@ -1,52 +1,37 @@
 import random
 import re
 import time
-answers = ['Бесспорно', 'Предрешено', 'Никаких сомнений', 'Определённо да', 'Можешь быть уверен в этом',
-          'Мне кажется - да', 'Вероятнее всего', 'Хорошие перспективы', 'Знаки говорят - да', 'Да',
-          'Пока неясно, попробуй снова', 'Спроси позже', 'Лучше не рассказывать', '	Сейчас нельзя предсказать',
-          'Сконцентрируйся и спроси опять', 'Даже не думай', 'Мой ответ - нет', 'По моим данным - нет',
-          'Перспективы не очень хорошие', 'Весьма сомнительно']
 
+answers = ['Бесспорно', 'Предрешено', 'Никаких сомнений', 'Определённо да', 'Можешь быть уверен в этом',
+           'Мне кажется - да', 'Вероятнее всего', 'Хорошие перспективы', 'Знаки говорят - да', 'Да',
+           'Пока неясно, попробуй снова', 'Спроси позже', 'Лучше не рассказывать', '	Сейчас нельзя предсказать',
+           'Сконцентрируйся и спроси опять', 'Даже не думай', 'Мой ответ - нет', 'По моим данным - нет',
+           'Перспективы не очень хорошие', 'Весьма сомнительно']
 
 print('Привет, Мир, я магический шар, и я знаю ответ на любой твой вопрос.')
 print('Ответом на вопрос должно быть да или нет')
 
-def has_cyrillic(text):
-    for i in text:
-        if not bool(re.search('[а-яА-Я]', i)):
-            return False
-        else:
-            text = text.rstrip()
-            return text
 
 def meeting():
     while True:
         a = input('Как тебя зовут? \n')
-        for i in a:
-            if i == ' ':
-                a = a.strip()
-            else:
-                a = a.rstrip()
-        if a.isdigit():
+        a = " ".join(a.split())
+        if any(letter.isdigit() for letter in a):
             print('А давно цифры стали именами?')
-        elif a.isalpha() and has_cyrillic(a):
-            print('Здравствуй,', a)
-            break
-        else:
+        elif any(letter for letter in a if not re.search('[а-яА-Я]', letter)):
             print('Какое то неправильное имя, давай без приколов')
+        else:
+            print('Здравствуй,', a)
+            return
 
 
 def asking():
     while True:
         c = input('Спрашивай, что хочешь, а я отвечу \n')
-        for i in c:
-            if i == ' ':
-                c = c.strip()
-            else:
-                c = c.rstrip()
-        if c[len(c) - 1:] == '?' and all(x.isspace() or x.isalnum() for x in c) and has_cyrillic(c):
+        " ".join(c.split())
+        if any(letter for letter in c if re.search('[а-яА-Я|\\s?$]', letter)):
             print(random.choice(answers))
-            break
+            return
         else:
             print('Это вовсе не вопрос')
 
@@ -54,15 +39,11 @@ def asking():
 def repeat():
     while True:
         b = input('Если хочешь задать еще вопрос, нажми "д", если нет - "н" \n')
-        for i in b:
-            if i == ' ':
-                b = b.strip()
-            else:
-                b = b.rstrip()
+        " ".join(b.split())
         if b == 'н':
             print('Возвращайся, если возникнут вопросы!')
             time.sleep(2)
-            break
+            return
         elif b == 'д':
             asking()
         else:
